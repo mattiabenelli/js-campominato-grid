5c97365dcd7c99a8d111db3fa193a0c4a844a722
mattiabenelli <benellimattia0@gmail.com>
2022-12-06 16:59:34 +0100

    griglia base fatta

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert("a");
+
+function createGridSquare(){
+    const elemento = document.createElement('div');
+    elemento.classList.add('square')
+    return elemento;
+}
+
+let grid = document.getElementById('grid')
+
+for( i=0; i<100; i++){
+    const currentSquare = createGridSquare();
+    grid.appendChild(currentSquare);
+}
