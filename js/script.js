7a265d4e906dec5062c41763a644d6bcee37ddb8
mattiabenelli <benellimattia0@gmail.com>
2022-12-06 17:14:15 +0100

    elementi griglia che cambiano colore al click

--- js/script.js
+++ js/script.js
@@ -4,10 +4,17 @@ function createGridSquare(){
     elemento.classList.add('square')
     return elemento;
 }
+function randomNumbers(min, max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
 let grid = document.getElementById('grid')
 
 for( i=0; i<100; i++){
     const currentSquare = createGridSquare();
     grid.appendChild(currentSquare);
+    
+    currentSquare.addEventListener('click',function(){
+        this.classList.add('clicked');
+    })
 }
